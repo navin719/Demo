a88e48666e297d71e1d8617f3ad93e58a05f5693
DIR\navin.t.chirumalla <navin.t.chirumalla@accenture.com>
2024-06-26 16:41:35 +0530

    @Pathvariable implementation

--- src/main/java/com/example/demo/RestTemplate.java
+++ src/main/java/com/example/demo/RestTemplate.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,12 @@ public class RestTemplate {
 		return "Welcome Navin..!! you are ready with your Azure project with CI/CD ";
 	}
 
+	
+
+	@GetMapping("/welcome/{name}")
+	String getName(@PathVariable String name){
+//		return "Welcome "+name+"..!! you are ready with your Azure project with CI/CD ";
+		return String.format("Welcome %s ..!! Finally you did it Congratulations ..!!!! hurray...:)", name);
+		
+	}
 }
