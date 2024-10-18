861e43ba302516888f608f8b4a38eabda9d4d114
Navinkumar Chirumalla <navinkumar.c@accelya.com>
2024-10-18 22:49:32 +0530

    Auto stash before revert of "Scenario 2"

--- src/main/java/com/example/demo/RestTemplate.java
+++ src/main/java/com/example/demo/RestTemplate.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestTemplate {
 	
-	@GetMapping("/test")
+	@GetMapping("/test1")
 	String getName(){
 		return "Welcome Navin..!! you are ready with your Azure project with CI/CD ";
 	}
 
 	
 
-	@GetMapping("/welcome/{NAMEScenario1}")
+	@GetMapping("/welcome/{NAMEScenario3}")
 	String getName(@PathVariable String name){
 //		return "Welcome "+name+"..!! you are ready with your Azure project with CI/CD ";
 		return String.format("Welcome %s ..!! Finally you did it Congratulations ..!!!! hurray...:)", name);
